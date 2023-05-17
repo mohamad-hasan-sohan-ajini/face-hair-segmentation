e086bdec1aa20165e60d0e0ce5e7aad08c006f38
hajix <sergioajini@gmail.com>
2023-05-17 16:57:16 +0330

    Initial commit

---
+++ train.py
@@ -0,0 +1,38 @@
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+from data import SegmentationDataModule, train_transforms, val_transforms
+from model import UNet
+# from model_ce import NNet
+
+# data module
+datamodule = SegmentationDataModule(
+    dataset_path_regex='data/lfw-funneled/lfw_funneled/*/*.jpg',
+    labels_base_path='data/parts_lfw_funneled_gt_images',
+    batch_size=32,
+    num_workers=8,
+    train_transforms=train_transforms,
+    val_transforms=val_transforms,
+)
+datamodule.setup()
+
+# model
+model = UNet(in_channels=3, out_channels=3, init_features=32)
+
+# callbacks
+checkpoint_callback = ModelCheckpoint(
+    save_top_k=3,
+    monitor='val_accuracy',
+    mode='max',
+    save_last=True,
+    every_n_train_steps=100,
+)
+
+# trainer and start training
+trainer = Trainer(
+    devices=1,
+    max_epochs=10,
+    callbacks=[checkpoint_callback],
+    # precision=16,
+)
+trainer.fit(model, datamodule)
